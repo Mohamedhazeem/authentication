fe40305ee84c604c08f3046f4612550c45ea87cf
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-10-02 14:47:25 +0530

    react router,nodemon and types added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./frontend/App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={route} />
+  </React.StrictMode>
+);
