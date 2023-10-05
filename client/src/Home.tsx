e7061c3424736939845d3c2b240d1a3c0f619670
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-10-05 15:50:40 +0530

    tailwind fix

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,3 +1,11 @@
 export const Home = () => {
-  return <></>;
+  return (
+    <>
+      <div className="bg-slate-300 h-10 pl-10 flex flex-row items-center">
+        <p className="text-orange-500 text-3xl font-bold ">
+          MERN AUTHENTICATION
+        </p>
+      </div>
+    </>
+  );
 };
