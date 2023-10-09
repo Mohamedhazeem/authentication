191eb3440c8db773fd5a46625ae8f35e9c30b8d1
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-10-09 19:17:05 +0530

    post sign up credentials to server

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -10,24 +10,30 @@ export const Home = () => {
   };
   return (
     <>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-100 w-80 h-[40%] rounded-xl flex flex-col justify-center items-center gap-4 ">
-        <BsPersonCircle size={50} />
-        <div className="flex flex-col justify-center items-center gap-10">
-          <button
-            className="bg-green-400 w-48 text-lg font-semibold rounded-xl"
-            onClick={() => showSignUp()}
-          >
-            CREATE AN ACCOUNT
-          </button>
-          <div className="flex flex-col justify-center items-center gap-2">
-            <p>Already have an account?</p>
-            <button className="bg-blue-400 w-24 text-lg font-semibold rounded-xl">
-              SIGN IN
+      {!isShowSignUp && (
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-100 w-80 h-[40%] rounded-xl flex flex-col justify-center items-center gap-4 ">
+          <BsPersonCircle size={50} />
+          <div className="flex flex-col justify-center items-center gap-10">
+            <button
+              className="bg-green-400 w-48 text-lg font-semibold rounded-xl"
+              onClick={() => showSignUp()}
+            >
+              CREATE AN ACCOUNT
             </button>
+            <div className="flex flex-col justify-center items-center gap-2">
+              <p>Already have an account?</p>
+              <button className="bg-blue-400 w-24 text-lg font-semibold rounded-xl">
+                SIGN IN
+              </button>
+            </div>
           </div>
         </div>
-        <SignUp isShow={isShowSignUp} />;
-      </div>
+      )}
+      {isShowSignUp && (
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-100 w-80 h-[40%] rounded-xl ">
+          <SignUp isShow={isShowSignUp} />
+        </div>
+      )}
     </>
   );
 };
--- client/src/SignUp.tsx
+++ client/src/SignUp.tsx
@@ -1,28 +1,68 @@
 import { BsPersonCircle } from "react-icons/bs";
+import axios from "axios";
+import { useState } from "react";
 type check = {
   isShow: boolean;
 };
 export const SignUp = (isShow: check) => {
-  console.log(isShow);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    const options = {
+      headers: { "content-type": "application/json" },
+    };
+    axios
+      .post(
+        " http://localhost:3000/signup",
+        {
+          email: email,
+          password: password,
+        },
+        options
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(async (error: Error) => {
+        const errorMessage = await error.message;
+        console.log(errorMessage);
+        const data = { msg: `User with email ${email} already exists` };
+        console.log(data);
+      });
   };
   return (
     <>
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-200 w-80 h-[100%] rounded-xl flex flex-col justify-center items-center gap-2 ">
-        <BsPersonCircle size={50} />
-        <form
-          className="flex flex-col justify-center items-start"
-          onSubmit={handleSubmit}
-        >
-          <label htmlFor="email">Email Address:</label>
-          <input type="email" id="email" placeholder="xyz@gg.com" required />
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" placeholder="*-*-" required />
-
-          <button type="submit">Create</button>
-        </form>
-      </div>
+      {isShow && (
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-200 w-80 h-[100%] rounded-xl flex flex-col justify-center items-center gap-2 ">
+          <BsPersonCircle size={50} />
+          <form
+            className="flex flex-col justify-center items-start"
+            onSubmit={handleSubmit}
+          >
+            <label htmlFor="email">Email Address:</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="xyz@gg.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="*-*-"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button type="submit">Create</button>
+          </form>
+        </div>
+      )}
     </>
   );
 };
