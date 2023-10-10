911a3f3401939304f5bae354c5d6215e57e9982b
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-10-10 20:16:30 +0530

    sign up controller

--- server/controllers/signUpController.ts
+++ server/controllers/signUpController.ts
@@ -2,6 +2,13 @@ import { Request, Response } from "express";
 import { authModel } from "../models/authModel";
 import { Error } from "mongoose";
 
+export const enum AlertEnum {
+  none,
+  signUp,
+  signed,
+  logedIn,
+}
+
 export const signUpController = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
@@ -9,11 +16,11 @@ export const signUpController = async (req: Request, res: Response) => {
       email: email});
 
     if (existUser) {
-      return res.json({ msg: `User with email ${email} already exists` });
+      return res.json({isExist:AlertEnum.signed, msg: `User with email ${email} already exists` });
     } else {
       const user = new authModel({ email: email, password: password });
       const savedUser = await user.save();
-      res.json(savedUser);
+      res.json({isExist:AlertEnum.signUp, msg: `Account created successfully` });
     }
   } catch {
     res.status(500).json({ msg: `Error creating user` });
