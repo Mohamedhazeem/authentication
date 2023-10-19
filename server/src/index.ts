8353eb9b8f2c871867fb6b199128771dcefa3bd6
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2023-10-19 11:15:38 +0530

    changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,12 @@ import 'dotenv/config';
 import cors from 'cors';
 import { signUpController } from '../controllers/signUpController';
 import { loginController } from '../controllers/loginController';
-import { authModel } from '../models/authModel';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
 mongoose.connect(process.env.DATABASE!).then(
     ()=>app.listen(3000, () => {console.log('connected')})
 ).catch(()=> console.log('Failed to connect'))
